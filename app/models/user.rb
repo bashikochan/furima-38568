06b43ddc1b9bb1c83bd7f6ffeb3f8bd3272bf496
bashikochan <bashikojob325@gmail.com>
2022-11-02 20:24:36 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth, presence: true
-
+  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid.Include both letters and numbers" }
+  
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid.Input full-width characters" }
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid.Input full-width characters" }
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters" } 
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters" } 
+  validates :birth, presence: true
 
 
 end
