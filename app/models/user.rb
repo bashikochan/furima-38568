ec6d543405dada41e634d55d0fedf0737db95ed5
bashikochan <bashikojob325@gmail.com>
2022-11-02 21:28:35 +0900

    ユーザーモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,8 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid.Include both letters and numbers' }
 
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid.Input full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid.Input full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: 'is invalid.Input full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: 'is invalid.Input full-width characters' }
   validates :last_name_kana, presence: true,
                              format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid.Input full-width katakana characters' }
   validates :first_name_kana, presence: true,
