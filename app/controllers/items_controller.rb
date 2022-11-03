b37294b24dab9cdc7b0715c1e0903fb031f4d830
bashikochan <bashikojob325@gmail.com>
2022-11-03 15:51:28 +0900

    ActiveHashを使用する項目の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path    
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :shipping_fee_id, :prefectures_id, :shipping_date)
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefectures
+  belongs_to :shipping_date
+
+  # ジャンル選択が「ーーー」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank"} 
+
 end
