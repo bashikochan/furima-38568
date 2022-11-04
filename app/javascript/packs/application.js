e9ec8b54a6951831f67f42d0cc17a03c2b49a059
bashikochan <bashikojob325@gmail.com>
2022-11-04 16:26:23 +0900

    販売手数料や販売利益の計算結果表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -9,6 +10,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//require('jquery')
+require("../tax")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,3 +20,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
